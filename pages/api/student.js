96704a2170eb479d084cf0e0616639d48e530f34
DreamIce-1 <christiangazzingan38@gmail.com>
2023-01-05 08:11:07 +0800

    ah shit here we go again

--- pages/api/student.js
+++
@@ -1,8 +0,0 @@
-export default function getAllStudent(req, res) {
-  if (req.method != "GET") {
-    res.status(500).json({message: "sorry get only "})
-  }
-  res.status(200).json({
-    data: "data"
-  })
-};
