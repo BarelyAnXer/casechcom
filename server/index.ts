09f2174f97d294c921dc2d30c4fbf76244bc09d9
DreamIce-1 <christiangazzingan38@gmail.com>
2023-01-05 21:34:13 +0800

    semi finish login register

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import pool from "./db";
 import cors from "cors";
+import bcrypt, {hash} from "bcrypt"
+import {FieldPacket, RowDataPacket} from "mysql2";
 
 dotenv.config();
 
@@ -14,6 +16,42 @@ app.use(cors())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.post("/register", async (req: Request, res: Response) => {
+  const {firstname, password} = req.body
+  await bcrypt.hash(password, 10, async (err, hash) => {
+      if (err) {
+        res.json({err})
+      }
+      const [rows, fields] = await pool.execute('insert into student (firstname,lastname, password) VALUES (?, ?, ?)', [firstname,
+        "test",
+        hash])
+      res.json({rows});
+    }
+  );
+
+});
+
+interface IUser extends RowDataPacket {
+  password: string
+}
+
+app.post("/login", async (req: Request, res: Response) => {
+  const {username, password} = req.body
+  const [rows, fields]: [IUser[], FieldPacket[]] = await pool.execute<IUser[]>("select * from student where firstname = ?", [username])
+
+  if (rows.length > 0) {
+    bcrypt.compare(password, rows[0].password, (error, result) => {
+      if (result) {
+        res.json({rows});
+      } else {
+        res.json({err: "wrong credentials"})
+      }
+    })
+  } else {
+    res.json({err: "wrong credentials"})
+  }
 });
 
 app.get("/test", async (req: Request, res: Response) => {
--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,76 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
-  const [state, setState] = useState("");
+  // const [state, setState] = useState("");
+  const [usernameRegister, setUsernameRegister] = useState("");
+  const [passwordRegister, setPasswordRegister] = useState("");
+
+  const [usernameLogin, setUsernameLogin] = useState("");
+  const [passwordLogin, setPasswordLogin] = useState("");
+
+  const [status, setStatus] = useState("");
+
   useEffect(() => {
     const test = async () => {
       const response = await axios.get("http://localhost:5000/test");
-      setState(JSON.stringify(response.data))
+      console.log(response.data)
     }
     test()
   }, [])
 
+  const login = async () => {
+    const response = await axios.post("http://localhost:5000/login", {
+      username: usernameLogin,
+      password: passwordLogin
+    })
+    console.log(response.data)
+  }
+
+  const register = async () => {
+    const response = await axios.post("http://localhost:5000/register", {
+      firstname: usernameRegister,
+      password: passwordRegister
+    })
+    console.log(response.data)
+  }
+
   return (
     <>
-      {state}
+      <h1>Register</h1>
+      <input type="text"
+             onChange={(event) => {
+               setUsernameRegister(event.target.value);
+             }
+             }/><br/>
+      <input type="text"
+             onChange={(event) => {
+               setPasswordRegister(event.target.value);
+             }
+             }/><br/>
+      {/*<select name="cars"*/}
+      {/*        id="cars">*/}
+      {/*  <option value="Student">Student</option>*/}
+      {/*  <option value="Teacher">Teacher</option>*/}
+      {/*</select><br/>*/}
+      <button onClick={register}>register</button>
+
+      <h1>Login</h1>
+
+      <input type="text"
+             onChange={(event) => {
+               setUsernameLogin(event.target.value);
+             }}/><br/>
+      <input type="text"
+             onChange={(event) => {
+               setPasswordLogin(event.target.value);
+             }}/><br/>
+      <button onClick={login}>login</button>
+
+      <h1>{status}</h1>
+
     </>
-  )
+  );
+
 }
 
 export default App
